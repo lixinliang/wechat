2e00e88ebdd2749585afb6f6233a84e19270b55b
李昕亮 <lixinliang@yy.com>
2017-01-11 09:52:52 +0800

    update
    referrer

--- src/entry/modules/referrer.js
+++ src/entry/modules/referrer.js
@@ -13,21 +13,17 @@ if (referrer) {
 if (referrer) {
     // web-app restart, get information from sessionStorage
     sdk.log.occur('referrer-restart');
-    location.hash = process.env.NODE_ENV === 'production' ? referrer.hash : '';
 } else {
-    // web-app first launch, get information from location hash
+    // web-app first launch, get information from window.name
     // or back to app from homepage
     sdk.log.occur('referrer-launch');
-    let hash = location.hash;
-    if (hash.length > 1) {
+    if (window.name.length > 1) {
         sdk.loading.report('正在读取路由信息...');
         try {
-            let temp = decodeURIComponent(hash.replace(/^#micro-app=/, ''));
+            let temp = decodeURIComponent(window.name.replace(/^micro-app=/, ''));
             referrer = JSON.parse(temp);
             sdk.loading.report('路由信息读取成功...');
             sessionStorage.setItem('referrer', temp);
-            sdk.loading.report('正在还原锚点...');
-            location.hash = process.env.NODE_ENV === 'production' ? referrer.hash : '';
             sdk.loading.report('正在获取应用图标...');
             if (referrer.query) {
                 app.name = referrer.query.name;
@@ -37,6 +33,7 @@ if (referrer) {
                     sdk.loading.report('应用图标获取成功...');
                 }
             }
+            window.name = '';
         } catch ( err ) {
             // parse fail, it must be something wrongs
             sdk.log.error('referrer-parse-fail', err);
