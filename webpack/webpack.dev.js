cc3633f7e3fb9f0617b430c79821ac438fce7cf8
李昕亮 <lixinliang@yy.com>
2017-01-30 23:38:32 +0800

    update webpack
    update webpack

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,6 +1,8 @@
 'use strict';
 
+let fs = require('fs');
 let path = require('path');
+let fse = require('fs-extra');
 let webpack = require('webpack');
 let autoprefixer = require('autoprefixer');
 let ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
@@ -10,6 +12,7 @@ const packageJson = require('../package.json');
 
 const alias = {};
 const imageSize = 10240;
+const sourcePath = path.join(__dirname, '../src');
 const constant = {
     NAME : packageJson.name,
     VERSION : packageJson.version,
@@ -31,6 +34,10 @@ let config = {
             {
                 test : /\.vue$/,
                 loader : 'vue',
+            },
+            {
+                test : /\.html$/,
+                loader : 'raw',
             },
             {
                 test : /\.(png|jpg|gif|svg)$/,
@@ -84,6 +91,69 @@ let config = {
     postcss () {
         return [autoprefixer({ browsers : ['last 2 versions'] })];
     },
+    devServer : {
+        proxy : {
+            '/' : {
+                bypass ( req, res ) {
+                    if (/^\/package\.json($|\?)/.test(req.url)) {
+                        res.writeHead(200, { 'Content-Type' : 'application/json' });
+                        res.end(getPackageJson());
+                    } else {
+                        return req.url;
+                    }
+                },
+            },
+        },
+    },
 };
+
+function getPackageJson () {
+
+    if (getPackageJson.result) {
+        return getPackageJson.result;
+    }
+
+    let result = {};
+
+    process.argv.forEach(( param ) => {
+        if (/^--/.test(param)) {
+            let temp = param.slice(2).split('=');
+            let key = temp[0];
+            let value = temp[1] || true;
+            process.argv[key] = value;
+        }
+    });
+
+    let version = ['x', 'x', 'x'];
+    const regexp = /^((^|\.)(0|([1-9](\d+)?))){1,3}$/;
+    if (process.argv.version && regexp.test(process.argv.version)) {
+        process.argv.version.split('.').forEach(( value, index ) => {
+            version[index] = value;
+        });
+    }
+
+    let versionRegexp = version.slice();
+    versionRegexp.forEach(( value, index ) => {
+        if (value === 'x') {
+            versionRegexp[index] = '(0|[1-9](\\d+)?)';
+        }
+    });
+    versionRegexp = new RegExp('^' + versionRegexp.join('\\.') + '$');
+
+    let packagePath = path.join(sourcePath, 'package');
+    fs.readdirSync(packagePath).forEach(( packageVersion ) => {
+        let packageVersionJs = path.join(packagePath, packageVersion, 'index.js');
+        let packageVersionJson = path.join(packagePath, packageVersion, 'package.json');
+        if (fs.existsSync(packageVersionJson) && fs.statSync(packageVersionJson).isFile()) {
+            if (versionRegexp.test(packageVersion)) {
+                result[packageVersion] = fse.readJsonSync(packageVersionJson);
+                result[packageVersion]['timestamp'] = result[packageVersion]['timestamp'] || null;
+            }
+        }
+    });
+
+    return getPackageJson.result = JSON.stringify(result);
+
+}
 
 module.exports = config;
